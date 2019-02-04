52e3e22b1c433fb7c3902acc46d6f3ac2c6fc426
Stephen Newell <sjnewell@gmail.com>
2019-02-03 18:51:43 -0700

    Update path to cache loader

--- lib/devpipeline_build/build.py
+++ lib/devpipeline_build/build.py
@@ -4,7 +4,7 @@
 import argparse
 
 import devpipeline_core.command
-import devpipeline_configure.cache
+import devpipeline_configure.load
 
 import devpipeline_build.builder
 
@@ -46,7 +46,7 @@ class BuildCommand(devpipeline_core.command.TaskCommand):
             super().process(arguments)
 
 
-def main(args=None, config_fn=devpipeline_configure.cache.update_cache):
+def main(args=None, config_fn=devpipeline_configure.load.update_cache):
     # pylint: disable=missing-docstring
     builder = BuildCommand(config_fn)
     # pylint: disable=missing-docstring
